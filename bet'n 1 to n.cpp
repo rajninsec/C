b39c39fde8df1d7ed67f1b3e81753f2374bb360a
rajninsec <47295306+rajninsec@users.noreply.github.com>
2019-10-22 18:10:12 +0530

    Add files via upload

---
+++ bet'n 1 to n.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int i,n;
+	for(i=1;i<=n;i++)
+	{
+		if(i%2==0)
+		{
+		printf("even no.");}
+		else
+		{
+		printf("odd no.");}
+    
+    printf("\n");}
+	return 0;
+}
